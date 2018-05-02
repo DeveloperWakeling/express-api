0d10a229681e9838dc78a52fc50b9ece71d6b57d
Michael W <michael@developerwakeling.com>
2018-05-01 22:06:18 -0400

    Added basic / route

--- app/routes/server_routes.js
+++ app/routes/server_routes.js
@@ -1,4 +1,8 @@
 module.exports = function (app, db) {
+    app.get('/', (req, res) => {
+        res.send('Working');
+    })
+    
     app.post('/test', (req, res) => {
         res.send('Hello');
     });
