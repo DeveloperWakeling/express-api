15552fada8777e7d25b395ecdf01807605f7a159
Michael W <michael@developerwakeling.com>
2018-05-15 23:00:51 -0400

    Change to Mongoose

--- server.js
+++ server.js
@@ -1,21 +1,44 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 //Needs to be a var because it can change
 var db = require('./config/db');
-
+var router = express.Router(); 
 const app = express();
 const port = 3000;
+var User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect(db.url);
+var db = mongoose.connection;
 
-MongoClient.connect(db.url, (err, client) => {
-    if (err) console.log(err)
-    db = client.db("tester");
-    require('./app/routes')(app, db);
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
 
-    app.listen(port, () => {
-        console.log("We are Live");
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+router.route('/user')
+    .post((req, res) => {
+        let user = new User();
+        user.username = req.body.username;
+        user.password = req.body.password;
+        user.email = req.body.email;
+
+        user.save((err) => {
+            if(err){
+                res.send(err);
+            }
+            res.send({"message" : "User Was Created"});
+        })
     });
 
+app.use('/api', router);
+
+app.listen(port, () => {
+    console.log("We are Live");
 });
